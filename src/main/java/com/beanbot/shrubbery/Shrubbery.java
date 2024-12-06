7d2d445744be11bcab31176ee44efe0aba9a524b
Ben Drape <btdrape@gmail.com>
2024-12-06 15:20:53 -0500

    Mod File Creation

---
+++ src/main/java/com/beanbot/shrubbery/Shrubbery.java
@@ -0,0 +1,23 @@
+package com.beanbot.shrubbery;
+
+import com.beanbot.shrubbery.common.items.ShrubberyItems;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Shrubbery.MODID)
+public class Shrubbery {
+
+    public static final String MODID = "shrubbery";
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Shrubbery(IEventBus bus, Dist dist) {
+
+        ShrubberyItems.ITEMS.register(bus);
+
+    }
+
+}
