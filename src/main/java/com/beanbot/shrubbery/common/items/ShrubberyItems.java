7b6a47f14ff7ee1af2200fd28e647c49683f3d34
Ben Drape <btdrape@gmail.com>
2024-12-09 15:23:07 -0500

    Add Item Registration

--- src/main/java/com/beanbot/shrubbery/common/items/ShrubberyItems.java
+++ src/main/java/com/beanbot/shrubbery/common/items/ShrubberyItems.java
@@ -9,5 +9,5 @@ public class ShrubberyItems {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Shrubbery.MODID);
 
-    public static final DeferredHolder<Item, Item> PRUNER = ITEMS.register("pruner", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final DeferredHolder<Item, PrunerItem> PRUNER = ITEMS.register("pruner", () -> new PrunerItem(new Item.Properties().stacksTo(1)));
 }
