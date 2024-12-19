c1e578174e9a367c00b63232f623caa7ce389511
Ben Drape <btdrape@gmail.com>
2024-12-19 16:15:04 -0500

    Azalea flower adjustments to particle

--- src/main/java/com/beanbot/shrubbery/common/blocks/AzaleaFlowerBlock.java
+++ src/main/java/com/beanbot/shrubbery/common/blocks/AzaleaFlowerBlock.java
@@ -25,12 +25,12 @@ public class AzaleaFlowerBlock extends FlowerBlock {
         double d0 = (double)pos.getX() + vec3.x;
         double d1 = (double)pos.getZ() + vec3.z;
 
-        for (int i = 0; i < 3; i++) {
-            if (random.nextBoolean()) {
+        for (int i = 0; i < 1; i++) {
+            if (random.nextInt(1, 5) == 1) {
                 level.addParticle(
-                        ParticleTypes.GUST,
+                        ParticleTypes.SPORE_BLOSSOM_AIR,
                         d0 + random.nextDouble() / 5.0,
-                        (double)pos.getY() + (0.5 - random.nextDouble()),
+                        (double)pos.getY() + (1.5 - random.nextDouble()),
                         d1 + random.nextDouble() / 5.0,
                         0.0,
                         0.0,
